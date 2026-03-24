edd237a4aa90251a373f4b510f1b49142f5c9784
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-03-24 16:02:45 +0530

    Learned different model fields

--- sample_app/models.py
+++ sample_app/models.py
@@ -5,12 +5,10 @@ from django.db import models
 from django.db import models
 
 class Student(models.Model):
-    name = models.CharField(max_length=100)
-    age = models.IntegerField()
-    email = models.EmailField()
-    address = models.TextField()
-    dob = models.DateField()
-    is_active = models.BooleanField(default=True)
+    name = models.CharField(max_length=100, default="No Name")
+    age = models.IntegerField(null=True, blank=True)
+    email = models.EmailField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
