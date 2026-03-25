eb86c5ed7f8e50c4e442d89f01071a674065c0cb
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-03-25 08:37:57 +0530

    applay migrations and see table in database

--- sample_app/models.py
+++ sample_app/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-
-
+from django.utils import timezone
 
 class Student(models.Model):
     name = models.CharField(max_length=100, default="No Name")
     age = models.IntegerField(null=True, blank=True)
-    email = models.EmailField(unique=True)
+    email = models.EmailField(unique=True, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
