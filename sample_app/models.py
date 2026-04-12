d5db926506f52a96aede0f1d249e737c683844a0
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-04-12 20:06:41 +0530

    write a models name :- Information

--- sample_app/models.py
+++ sample_app/models.py
@@ -17,3 +17,14 @@ class Student(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+
+class Information(models.Model):
+    name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
