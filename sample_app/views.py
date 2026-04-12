d41f8529094841b6a12428a70f6329b3a693414e
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-04-12 20:08:09 +0530

    Learn Function Base Apis 1. learn POST method and implement it

--- sample_app/views.py
+++ sample_app/views.py
@@ -1,6 +1,41 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
+from .models import Student , Information
+from django.views.decorators.csrf import csrf_exempt
+
 
 
 
 def home(request):
     return render(request,'sample_app/home.html')
+
+
+
+"""
+Author: Aliyan Shaikh
+Created On: 12-4-2026
+Description: Learn Function Base Apis 
+            1. learn POST method and implement it
+"""
+
+@csrf_exempt
+def post_data(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+
+        info = Information.objects.create(
+            name=data.get('name'),
+            username=data.get('username'),
+            password=data.get('password'),
+            city=data.get('city')
+        )
+
+        return JsonResponse({
+            'message': 'data post successfully',
+            'id': info.id
+        })
+
+    return JsonResponse({
+        'error': 'Only POST method allowed'
+    })
