5855460f785c125e741faf34241a36d317c9d5f3
Aliyanshaikh17 <aliyanshaikh817@gmail.com>
2026-04-13 16:46:25 +0530

    write url for (get_all_data) get apis

--- sample_app/urls.py
+++ sample_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
    path('home', views.home , name='home'),
-   path('post_data/' , views.post_data , name='post_data')
+   path('post_data/' , views.post_data , name='post_data'),
+   path('get_all_data/', views.get_all_data , name='get_all_data')
 ]
